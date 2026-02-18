b71811b25e1d9f066bbdb15cc23b1b1f359368bd
Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
2026-02-18 03:41:59 -0300

    Vista para mostrar las evoluciones medicas asociadas a una admision

--- controllers/views/admisionViewsController.js
+++ controllers/views/admisionViewsController.js
@@ -325,10 +325,67 @@ exports.getEstadia = async (req, res, next) => {
     });
     const alas = await Ala.findAll({ where: { activo: true } });
 
+    const vias = await ViaAdministracion.findAll({ where: { activo: true } });
     res.render('./Admision/Estadia.pug', {
       title: 'Gestionar Estadia',
       admision,
-      alas
+      alas,
+      vias
+    });
+  } catch (error) {
+    console.error(error)
+    next(error);
+  }
+};
+
+exports.getEvolucionMedica = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const pagina = parseInt(req.query.pagina, 10) || 1;
+    const { activo } = req.query;
+    const registrosPorPagina = 13;
+    const offset = (pagina - 1) * registrosPorPagina;
+
+    const admision = await Admision.findByPk(id, {
+      include: [
+        {
+          model: Paciente,
+          as: 'paciente',
+          include: [{ model: Identificacion, as: 'identificaciones' }]
+        },
+      ]
+    });
+
+    if (!admision) {
+      return res.redirect('/admisiones');
+    }
+
+    const whereClause = { admision_id: id };
+    if (activo !== undefined && activo !== '') whereClause.activo = activo === 'true';
+
+    const { count, rows: evoluciones } = await EvolucionMedica.findAndCountAll({
+      where: whereClause,
+      order: [['fecha_hora', 'DESC']],
+      limit: registrosPorPagina,
+      offset: offset
+    });
+
+    admision.evoluciones_medicas = evoluciones;
+
+    const totalPaginas = Math.ceil(count / registrosPorPagina);
+    const filtros = { activo };
+
+    res.render('./Estadia/EvolucionMedica.pug', {
+      title: 'Gestionar Evoluciones Medicas',
+      admision,
+      filtros,
+
+      paginacion: {
+        totalRegistros: count,
+        totalPaginas: totalPaginas,
+        paginaActual: pagina,
+        registrosPorPagina: registrosPorPagina
+      },
     });
   } catch (error) {
     console.error(error)
--- routes/views/admisionViewsRouter.js
+++ routes/views/admisionViewsRouter.js
@@ -9,6 +9,8 @@ router.get('/registrar', admisionController.getRegistrar)
 
 router.get('/:id/estadia', admisionController.getEstadia);
 
+router.get('/:id/evolucion-medica', admisionController.getEvolucionMedica);
+
 router.get('/:id', admisionController.getAdmision);
 
 module.exports = router;
